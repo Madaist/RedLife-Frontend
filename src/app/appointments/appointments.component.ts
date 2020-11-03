87fa02a17b3a03c22b52aab57940372aadf6e252
ASNET\mistrate <madalina.istrate@edenred.com>
2020-11-03 22:55:32 +0200

    Added Appointment component

---
+++ src/app/appointments/appointments.component.ts
@@ -0,0 +1,74 @@
+import { Component, Injector } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { appModuleAnimation } from '@shared/animations/routerTransition';
+import {
+  PagedListingComponentBase,
+  PagedRequestDto
+} from '@shared/paged-listing-component-base';
+import {
+  AppointmentServiceProxy,
+  AppointmentDto,
+  AppointmentDtoPagedResultDto
+} from '@shared/service-proxies/service-proxies';
+
+class PagedAppointmentsRequestDto extends PagedRequestDto {
+  keyword: string;
+}
+
+@Component({
+  templateUrl: './appointments.component.html',
+  animations: [appModuleAnimation()]
+})
+export class AppointmentsComponent extends PagedListingComponentBase<AppointmentDto> {
+  appointments: AppointmentDto[] = [];
+  keyword = '';
+
+  constructor(
+    injector: Injector,
+    private _appointmentsService: AppointmentServiceProxy,
+    private _modalService: BsModalService
+  ) {
+    super(injector);
+  }
+
+  list(
+    request: PagedAppointmentsRequestDto,
+    pageNumber: number,
+    finishedCallback: Function
+  ): void {
+    request.keyword = this.keyword;
+
+    this._appointmentsService
+      .getAll(request.keyword, request.skipCount, request.maxResultCount)
+      .pipe(
+        finalize(() => {
+          finishedCallback();
+        })
+      )
+      .subscribe((result: AppointmentDtoPagedResultDto) => {
+        this.appointments = result.items;
+        this.showPaging(result, pageNumber);
+      });
+  }
+
+  delete(appointment: AppointmentDto): void {
+    abp.message.confirm(
+      this.l('AppointmentDeleteWarningMessage'),
+      undefined,
+      (result: boolean) => {
+        if (result) {
+          this._appointmentsService
+            .delete(appointment.id)
+            .pipe(
+              finalize(() => {
+                abp.notify.success(this.l('SuccessfullyDeleted'));
+                this.refresh();
+              })
+            )
+            .subscribe(() => {});
+        }
+      }
+    );
+  }
+}
