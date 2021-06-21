6bd760e7fefbc5f97325fe381ee4a9fd38a270cc
ASNET\mistrate <madalina.istrate@edenred.com>
2021-06-21 21:23:13 +0300

    Small improvements

--- src/app/layout/sidebar-user-panel.component.ts
+++ src/app/layout/sidebar-user-panel.component.ts
@@ -5,6 +5,7 @@ import {
   OnInit
 } from '@angular/core';
 import { AppComponentBase } from '@shared/app-component-base';
+import { UserLoginInfoDto } from '@shared/service-proxies/service-proxies';
 
 @Component({
   selector: 'sidebar-user-panel',
@@ -14,12 +15,21 @@ import { AppComponentBase } from '@shared/app-component-base';
 export class SidebarUserPanelComponent extends AppComponentBase
   implements OnInit {
   shownLoginName = '';
-
+  loggedUser: UserLoginInfoDto;
+  isFemale: boolean = false;
   constructor(injector: Injector) {
     super(injector);
   }
 
   ngOnInit() {
     this.shownLoginName = this.appSession.getShownLoginName();
+    this.loggedUser = this.appSession.user;
+    var firstDigitOfId = this.loggedUser.id.toString().substring(0, 1);
+    console.log(firstDigitOfId);
+    if(this.isGranted("Donor") && (firstDigitOfId == "2" || firstDigitOfId == "6")){
+      console.log("a intrat aici");
+      this.isFemale = true;
+    }
+    console.log(this.isFemale);
   }
 }
